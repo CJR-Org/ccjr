0bca428cd34c443a1111ab0d36dc34859130133b
raxracks <multisniperism@gmail.com>
2022-04-11 15:51:03 +1200

    clean up, allow empty variable definitions

--- translate.js
+++ translate.js
@@ -3,21 +3,11 @@ import { requires_semicolon } from "./postprocess.js"
 function get_type(type, types) {
   let split = type.split(" ");
 
-  // let append = "";
-
-  // if (type.includes("*")) {
-    // append = "*";
-    // type = type.split("*").join("");
-  // }
-
-  for(let i = 0; i < split.length; i++) {
-    if(types[split[i]]) split[i] = types[split[i]];
+  for (let i = 0; i < split.length; i++) {
+    if (types[split[i]]) split[i] = types[split[i]];
   };
 
-  // if (types[type]) return types[type];
-  // return type;
   return split.join(" ");
-  // console.error(`Unknown Type: "${type}"`);
 }
 
 export function transpile(lines, types, verbose) {
@@ -31,32 +21,38 @@ export function transpile(lines, types, verbose) {
     line = line.trim();
     let new_line = "";
     let ns = [...namespace].reverse().join("_o_") + "_o_";
-    if(!namespace[0]) ns = "";
-    if(line.startsWith("//")) line = "";
+    if (!namespace[0]) ns = "";
+    if (line.startsWith("//")) line = "";
 
     line.split(/[\s\t]+/).forEach((word) => {
       if (word === "var") {
         const name = line.split(":")[0].split("var ")[1].trim();
         let type = line.split(":")[1];
         let value = line.split(":");
-        value.shift();
-        value = value.join(":");
-        value = value.split("=");
-        value.shift();
-        value = value.join("").trimStart();
-        type = type.split("=")[0].trim();
+        if (line.includes("=")) {
+          value.shift();
+          value = value.join(":");
+          value = value.split("=");
+          value.shift();
+          value = value.join("").trimStart();
+          type = type.split("=")[0].trim();
+        } else {
+          if(type.endsWith(";")) type = type.split(";")[0].trim();
+          else type = type.split("\n")[0].trim();
+          value = false;
+        }
 
-        if(verbose) console.log(`Found variable symbol: ${name} with type ${type} in namespace "${ns}"\n`);
+        if (verbose) console.log(`Found variable symbol: ${name} with type ${type} in namespace "${ns}"\n`);
 
-        new_line += `${get_type(type, types)} ${ns}${name} = ${value}`;
+        new_line += `${get_type(type, types)} ${ns}${name} ${value ? "=": ""} ${value ? value: ""}`;
       }
 
       if (word === "func") {
         const name = line.split("func ")[1].split("(")[0].trim();
         const type = line.split(")")[1].split(":")[1].split("{")[0].trim();
         let args = line.split("(")[1].split(")")[0].split(",");
-        if(args.length === 1) {
-          if(line.split("(")[1].split(")")[0].trim().length !== 0) {
+        if (args.length === 1) {
+          if (line.split("(")[1].split(")")[0].trim().length !== 0) {
             args = [line.split("(")[1].split(")")[0]];
           } else {
             args.pop();
@@ -71,59 +67,42 @@ export function transpile(lines, types, verbose) {
         });
 
 
-        if(verbose) console.log(`Found function symbol: ${name} with return type ${type} in namespace "${ns}" and args: ${translated_args.join(", ")}\n`);
+        if (verbose) console.log(`Found function symbol: ${name} with return type ${type} in namespace "${ns}" and args: ${translated_args.join(", ")}\n`);
 
         new_line += `${get_type(type, types)} ${ns}${name}(${translated_args.join(", ")}) {`;
       }
 
-      if(word.includes("::")) {
-        if(!new_line) new_line = line;
-        // console.log(new_line);
-        // let start = new_line.split("::")[0].split(" ")[1];
-        // console.log(start);
-        // if(start.includes('"') || start.includes("'")) {
+      if (word.includes("::")) {
+        if (!new_line) new_line = line;
 
         new_line = new_line.split("::").join("_o_");
 
-        if(verbose) console.log(`Found namespace member reference symbol: ${line} in namespace ${ns}, deobjectified to ${new_line}\n`);
+        if (verbose) console.log(`Found namespace member reference symbol: ${line} in namespace ${ns}, deobjectified to ${new_line}\n`);
       }
 
-      if(word == "namespace") {
+      if (word == "namespace") {
         const name = line.slice("namespace ".length).split("{")[0].trim();
         namespace_pos.push(brackets);
         namespace.unshift(name);
         line = "";
 
-        if(verbose) console.log(`Found namespace declaration symbol: ${name} in namespace "${ns}"\n`);
+        if (verbose) console.log(`Found namespace declaration symbol: ${name} in namespace "${ns}"\n`);
       }
 
-      if(word == "{") {
+      if (word == "{") {
         brackets++;
       }
 
-      if(word == "}") {
+      if (word == "}") {
         brackets--;
 
-        if(namespace_pos.includes(brackets)) {
+        if (namespace_pos.includes(brackets)) {
           namespace_pos.splice(namespace_pos.indexOf(brackets), 1);
           namespace.shift();
           line = "";
         }
       }
     });
-
-    // if(line.startsWith("}") && line.split(" ").length == 2 && line.length > 2 && !line.includes(")") && line.split("}").length == 2) {
-    //   const name = line.split("}")[1].split(";")[0].trim();
-
-    //   if(verbose) console.log(`Found closing namespace symbol: ${name} in namespace "${ns}"\n`);
-
-    //   if(namespace.includes(name)) {
-    //     namespace.shift();
-    //     line = "";
-    //   } else {
-    //     throw new Error(`Unknown namespace ${name}`);
-    //   }
-    // }
 
     if (!new_line) {
       if (requires_semicolon(line)) line += ";";
